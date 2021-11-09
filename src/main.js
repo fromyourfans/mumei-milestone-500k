06ff3b9a74fd00cc593cf483a8a5814804ad9689
dragonjet <jetriconew@gmail.com>
2021-11-08 22:50:40 -0800

    Install vue-masonry

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import { VueMasonryPlugin } from 'vue-masonry';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+
+Vue.use(VueMasonryPlugin);
 
 Vue.config.productionTip = false;
 
